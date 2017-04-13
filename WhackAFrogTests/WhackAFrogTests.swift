1834942cd5f11fc935eeb5f7e6a3157c983cbf38
matka <matanc4@mail.afeka.ac.il>
2017-04-13 12:52:25 +0300

    Initial Commit

---
+++ WhackAFrogTests/WhackAFrogTests.swift
@@ -0,0 +1,36 @@
+//
+//  WhackAFrogTests.swift
+//  WhackAFrogTests
+//
+//  Created by Matan on 13/04/2017.
+//  Copyright © 2017 Matan. All rights reserved.
+//
+
+import XCTest
+@testable import WhackAFrog
+
+class WhackAFrogTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
